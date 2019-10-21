79b66f0792085d5bc92682b7d5c0df0eeea1e114
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-21 19:26:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array = [1,2,3]
+  counter = 0
+  while array[counter] do
+    new_array << array[counter] ** 2
+    counter += 1
+    end
+    return new_array
 end
